a053cdc45f09f623d1f5acd041c3352a91814854
YasuoKatou <yasuokatou@gmail.com>
2018-07-07 11:07:55 +0900

    トランザクション内でDaoのテスト

--- README.md
+++ README.md
@@ -5,10 +5,16 @@
 ### できる事
 1. フィルターを複数指定可能（順番も指定可能）
 1. application.ymlに定義するパスワードなどの暗号化
+1. JUnit（トランザクション内でDaoのテストができる。DBデータはCSV形式が可能）
 
 ### 今後の修正（目標）
-1. JUnit
+1. JUnit（リクエスト／レスポンス）
 
 ### 起動方法
+(1) アプリケーションの起動
 <pre>$ ./mvnw -Dspring.main.banner-mode=off -Djasypt.encryptor.password=$PASSWORD spring-boot:run</pre>
+「$PASSWORD」は、暗号化時のパスワードを指定します。
+
+(2) JUnitの起動
+<pre>$ ./mvnw -Dspring.main.banner-mode=off -Djasypt.encryptor.password=$PASSWORD test</pre>
 「$PASSWORD」は、暗号化時のパスワードを指定します。
---
+++ spring_boot_demo/src/test/java/yksolution/demo/dao/UserMasterDaoTest.java
@@ -0,0 +1,61 @@
+package yksolution.demo.dao;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DbUnitConfiguration;
+
+import yksolution.demo.DemoApplication;
+import yksolution.demo.entity.UserMasterEntity;
+import yksolution.demo.test.common.CsvDataSetLoader;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {
+  DemoApplication.class,
+  UserMasterDao.class
+})
+@SpringBootTest
+@Transactional
+@TestExecutionListeners({
+  DependencyInjectionTestExecutionListener.class,
+  DirtiesContextTestExecutionListener.class,
+  TransactionDbUnitTestExecutionListener.class,
+  DbUnitTestExecutionListener.class})
+@DbUnitConfiguration(dataSetLoader = CsvDataSetLoader.class)
+/**
+ * ユーザ情報テーブルDaoのテストクラス
+ * @author Y.Katou
+ */
+public class UserMasterDaoTest {
+
+  @Autowired private UserMasterDao userMasterDao;
+
+  /**
+   * 指定のユーザを取得する.
+   * @since 0.0.1
+   */
+  @Test
+  @DatabaseSetup("classpath:UserMasterDaoTest_01/")
+  public void testFindUser() {
+    String userId = "hoge2";
+    UserMasterEntity userMaster = userMasterDao.findUser(userId);
+    assertNotNull("Entity", userMaster);
+    assertEquals("ユーザ名", userId, userMaster.getUserId());
+    assertEquals("パスワード", "page1", userMaster.getPasswd());
+    assertEquals("権限", "admin3", userMaster.getAuthority());
+  }
+}
